73f1f6e7b6b1b9ec9d08b8222ba6781f995429a0
Miriam <miernest@umich.edu>
2017-03-23 08:05:15 -0400

    fixed tabs

--- rpn.py
+++ rpn.py
@@ -1,42 +1,49 @@
 #!/usr/bin/env python3
 
+from termcolor import colored
+
 def add(arg1, arg2):
-	return arg1 + arg2
+    return arg1 + arg2
 
 def subtract(arg1, arg2):
-	return arg1 - arg2
+    return arg1 - arg2
 import operator
 
 OPERATORS = {
-	'+': add,
-	'-': subtract,
-	'+': operator.add,
-	'-': operator.sub,
-	'*': operator.mul,
-	'/': operator.truediv,
+    '+': add,
+    '-': subtract,
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
         '^': operator.pow,
 }
 
 
 def calculate(arg):
-	stack = list()
-	for operand in arg.split():
-		try:
-			operand = float(operand)
-			stack.append(operand)
-		except:
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			operator_fn = OPERATORS[operand]
-			result = operator_fn(arg1, arg2)
-			
-			stack.append(result)
-	return stack.pop()
+    stack = list()
+    for operand in arg.split():
+        try:
+            operand = float(operand)
+            stack.append(operand)
+        except:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            operator_fn = OPERATORS[operand]
+            result = operator_fn(arg1, arg2)
+            
+            stack.append(result)
+    return stack.pop()
 
 def main():
-	while True:
-		result = calculate(input('rpn calc> '))
-		print("Result:", result)
+    while True:
+        result = calculate(input('rpn calc> '))
+
+        if result < 0:
+            print("Result:", colored(result, 'red'))
+
+        else:
+            print("Result:", colored(result, 'green'))
 
 if __name__ == '__main__':
         main()
